9fcbfbbcbcc5056160c6df28ae8f8353396451f9
Brett <brett.castellanos@gmail.com>
2019-05-26 10:21:34 -0500

    Add db_setup.sql

---
+++ db_setup.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    kgs_id VARCHAR(64) UNIQUE NOT NULL,
+    ayd BOOLEAN DEFAULT false,
+    eyd BOOLEAN DEFAULT false
+);
+
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY,
+    black VARCHAR(64) REFERENCES users(kgs_id),
+    white VARCHAR(64) REFERENCES users(kgs_id),
+    b_win BOOLEAN DEFAULT false,
+    w_win BOOLEAN DEFAULT false,
+    tmstmp TIMESTAMP,
+    yd_game BOOLEAN DEFAULT false
+);
